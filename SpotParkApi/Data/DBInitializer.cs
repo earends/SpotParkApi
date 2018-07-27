a4bf31aa9aef225bb0653a64e1eccdd2c0a1c41f
Evan Arends <evanarendss@gmail.com>
2018-07-27 07:58:11 -0700

    Added data base intializer

---
+++ SpotParkApi/Data/DBInitializer.cs
@@ -0,0 +1,94 @@
+﻿using SpotParkApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotParkApi.Data
+{
+    public class DBInitializer
+    {
+        public static void Initialize(SpotParkApiContext context)
+        {
+            if (context.ParkingSpot.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var spots = new ParkingSpot[]
+            {
+                new ParkingSpot
+                {
+                    Title = "Northgate",
+                    Size = 4,
+                    Location = "Northgate",
+                    LocationIdentifier = "Thorton",
+                    SpecialInstructions = "Creek",
+                    Cost = 1,
+                    AvailabilityStart = DateTime.Now,
+                    AvailabilityEnd = DateTime.Now,
+                    Email = "test@gmail.com"
+
+                },
+                new ParkingSpot
+                {
+                    Title = "Ballard",
+                    Size = 3,
+                    Location = "Ballard",
+                    LocationIdentifier = "Near High School",
+                    SpecialInstructions = "Shit Creek",
+                    Cost = 2,
+                    AvailabilityStart = DateTime.Now,
+                    AvailabilityEnd = DateTime.Now,
+                    Email = "test1@gmail.com"
+
+                },
+                new ParkingSpot
+                {
+                    Title = "Eastlake",
+                    Size = 2,
+                    Location = "Eastlake",
+                    LocationIdentifier = "South Lake Union",
+                    SpecialInstructions = "Margaritas",
+                    Cost = 3,
+                    AvailabilityStart = DateTime.Now,
+                    AvailabilityEnd = DateTime.Now,
+                    Email = "test2@gmail.com"
+
+                },
+                new ParkingSpot
+                {
+                    Title = "Capital Hill",
+                    Size = 1,
+                    Location = "Capital Hill",
+                    LocationIdentifier = "Hippies",
+                    SpecialInstructions = "First Come First Serve",
+                    Cost = 5,
+                    AvailabilityStart = DateTime.Now,
+                    AvailabilityEnd = DateTime.Now,
+                    Email = "test3@gmail.com"
+
+                },
+                new ParkingSpot
+                {
+                    Title = "Everette",
+                    Size = 7,
+                    Location = "Everette",
+                    LocationIdentifier = "Comcast Arena",
+                    SpecialInstructions = "YMCA",
+                    Cost = 0,
+                    AvailabilityStart = DateTime.Now,
+                    AvailabilityEnd = DateTime.Now,
+                    Email = "test4@gmail.com"
+
+                }
+
+            };
+            foreach (ParkingSpot p in spots)
+            {
+                context.ParkingSpot.Add(p);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- SpotParkApi/Program.cs
+++ SpotParkApi/Program.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SpotParkApi.Data;
+using SpotParkApi.Models;
 
 namespace SpotParkApi
 {
@@ -14,8 +17,26 @@ namespace SpotParkApi
     {
         public static void Main(string[] args)
         {
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
 
-            CreateWebHostBuilder(args).Build().Run();
+                try
+                {
+                    var context = services.GetRequiredService<SpotParkApiContext>();
+                    //context.Database.EnsureCreated();
+                    DBInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+                host.Run();
+            
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
